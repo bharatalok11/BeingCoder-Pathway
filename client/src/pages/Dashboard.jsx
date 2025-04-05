25a650e10a58c20d04dcb623cd496e489713ec8b
Karankumar2004 <kk8594879@gmail.com>
2025-04-05 12:24:36 +0530

    Problem commite

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -8,6 +8,17 @@ import Track_booking from '../components/Track_booking'
 const Dashboard = () => {
     return (
         <div>
+            <div id="content" className='flex h-screen overflow-hidden'>
+                <div id="sidebar" className="flex flex-col h-screen w-64 bg-white dark:bg-gray-800 shadow-lg p-4">
+                    <div className="mb-6">
+                        <a href="/">
+                            <img src="logo.png" alt="Website Logo" className="h-10 mx-auto" />
+                        </a>
+                    </div>
+                </div>
+
+
+            </div>
             <Appointment />
             <Bed_availability />
             <Medicine_availability />
